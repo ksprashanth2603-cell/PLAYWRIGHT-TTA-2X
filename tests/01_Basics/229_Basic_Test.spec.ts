be774fb9ad0845a3046dea98483b8a0aba9b4bcd
ksprashanth2603-cell <ksprashanth2603@gmail.com>
2026-08-09 13:53:05 +0530

    Commit: update repository

--- tests/01_Basics/229_Basic_Test.spec.ts
+++ tests/01_Basics/229_Basic_Test.spec.ts
@@ -1,6 +1,6 @@
 import {test, expect} from '@playwright/test';
 
-test.only('To verify the Title of the page', async ({page}) => //page = Fixture (Injected by Playwright Test Runner)
+test('To verify the Title of the page', async ({page}) => //page = Fixture (Injected by Playwright Test Runner)
 {
     await page.goto('https://app.vwo.com/');
     await expect(page).toHaveTitle('Login - Wingify');
