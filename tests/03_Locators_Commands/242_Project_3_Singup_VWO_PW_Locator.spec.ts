ba23df35a59d8802e17b258f537a5eede1988cb8
ksprashanth2603-cell <ksprashanth2603@gmail.com>
2026-08-10 19:19:47 +0530

    Added my own test changes

---
+++ tests/03_Locators_Commands/242_Project_3_Singup_VWO_PW_Locator.spec.ts
@@ -0,0 +1,17 @@
+import {test, expect} from 'playwright/test';
+
+test('Verify the sign up error with invalid email with PW locator', async ({ page }) => 
+    {
+    await page.goto("https://vwo.com/free-trial/");
+    page.getByRole('textbox', { name: 'Email' }).fill("test@");
+
+    await page.getByRole('checkbox').check();
+    await page.getByRole('button', { name: 'Create a Free Trial Account' }).click();
+
+    let error_msg = await page.getByText("The email address you entered is incorrect.").textContent();
+    expect(error_msg).toContain("The email address you entered is incorrect.");
+    //let error_msg = await page.getByText("The email address you entered is incorrect.").textContent();
+    //expect(error_msg).toContain("The email address you entered is incorrect.");
+    }
+    
+    );
